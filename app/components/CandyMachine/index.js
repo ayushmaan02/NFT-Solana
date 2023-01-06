e7347ad34af3b54bac86769dfe6970b6e44c834c
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2023-01-06 19:03:59 +0530

    Updated code

--- app/components/CandyMachine/index.js
+++ app/components/CandyMachine/index.js
@@ -44,97 +44,6 @@ const CandyMachine = ({ walletAddress }) => {
         )[0];
     };
 
-    useEffect(() =>{
-        getCandyMachineState();
-    }, []);
-
-    const getProvider = () => {
-        const rpcHost = process.env.NEXT_PUBLIC_SOLANA_RPC_HOST;
-        // Create a new connection object
-        const connection = new Connection(rpcHost);
-        
-        // Create a new Solana provider object
-        const provider = new AnchorProvider(
-          connection,
-          window.solana,
-          opts.preflightCommitment
-        );
-      
-        return provider;
-      };
-      // Declare getCandyMachineState as an async method
-    const getCandyMachineState = async () => {
-        const provider = getProvider();
-        
-        // Get metadata about your deployed candy machine program
-        const idl = await Program.fetchIdl(candyMachineProgram, provider);
-    
-        // Create a program that you can call
-        const program = new Program(idl, candyMachineProgram, provider);
-    
-        // Fetch the metadata from your candy machine
-        const candyMachine = await program.account.candyMachine.fetch(
-        process.env.NEXT_PUBLIC_CANDY_MACHINE_ID
-        );
-        
-        // Parse out all our metadata and log it out
-        const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
-        const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
-        const itemsRemaining = itemsAvailable - itemsRedeemed;
-        const goLiveData = candyMachine.data.goLiveDate.toNumber();
-        const presale =
-        candyMachine.data.whitelistMintSettings &&
-        candyMachine.data.whitelistMintSettings.presale &&
-        (!candyMachine.data.goLiveDate ||
-            candyMachine.data.goLiveDate.toNumber() > new Date().getTime() / 1000);
-        
-        // We will be using this later in our UI so let's generate this now
-        const goLiveDateTimeString = `${new Date(
-            goLiveData * 1000
-          ).toLocaleDateString()} - ${new Date(
-            goLiveData * 1000
-          ).toLocaleTimeString()}`;
-
-          setCandyMachine({
-            id: process.env.NEXT_PUBLIC_CANDY_MACHINE_ID,
-            program,
-            state: {
-              itemsAvailable,
-              itemsRedeemed,
-              itemsRemaining,
-              goLiveData,
-              goLiveDateTimeString,
-              isSoldOut: itemsRemaining === 0,
-              isActive:
-                (presale ||
-                  candyMachine.data.goLiveDate.toNumber() < new Date().getTime() / 1000) &&
-                (candyMachine.endSettings
-                  ? candyMachine.endSettings.endSettingType.date
-                    ? candyMachine.endSettings.number.toNumber() > new Date().getTime() / 1000
-                    : itemsRedeemed < candyMachine.endSettings.number.toNumber()
-                  : true),
-              isPresale: presale,
-              goLiveDate: candyMachine.data.goLiveDate,
-              treasury: candyMachine.wallet,
-              tokenMint: candyMachine.tokenMint,
-              gatekeeper: candyMachine.data.gatekeeper,
-              endSettings: candyMachine.data.endSettings,
-              whitelistMintSettings: candyMachine.data.whitelistMintSettings,
-              hiddenSettings: candyMachine.data.hiddenSettings,
-              price: candyMachine.data.price,
-            },
-          }); 
-
-        console.log({
-        itemsAvailable,
-        itemsRedeemed,
-        itemsRemaining,
-        goLiveData,
-        goLiveDateTimeString,
-        presale,
-        });
-  };
-
     const createAssociatedTokenAccountInstruction = (associatedTokenAddress, payer, walletAddress, splTokenMintAddress) => {
         const keys = [
             { pubkey: payer, isSigner: true, isWritable: true },
@@ -323,6 +232,96 @@ const CandyMachine = ({ walletAddress }) => {
         }
         return [];
     };
+    useEffect(() =>{
+        getCandyMachineState();
+    }, []);
+
+    const getProvider = () => {
+        const rpcHost = process.env.NEXT_PUBLIC_SOLANA_RPC_HOST;
+        // Create a new connection object
+        const connection = new Connection(rpcHost);
+        
+        // Create a new Solana provider object
+        const provider = new AnchorProvider(
+          connection,
+          window.solana,
+          opts.preflightCommitment
+        );
+      
+        return provider;
+      };
+      // Declare getCandyMachineState as an async method
+    const getCandyMachineState = async () => {
+        const provider = getProvider();
+        
+        // Get metadata about your deployed candy machine program
+        const idl = await Program.fetchIdl(candyMachineProgram, provider);
+    
+        // Create a program that you can call
+        const program = new Program(idl, candyMachineProgram, provider);
+    
+        // Fetch the metadata from your candy machine
+        const candyMachine = await program.account.candyMachine.fetch(
+        process.env.NEXT_PUBLIC_CANDY_MACHINE_ID
+        );
+        
+        // Parse out all our metadata and log it out
+        const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
+        const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
+        const itemsRemaining = itemsAvailable - itemsRedeemed;
+        const goLiveData = candyMachine.data.goLiveDate.toNumber();
+        const presale =
+        candyMachine.data.whitelistMintSettings &&
+        candyMachine.data.whitelistMintSettings.presale &&
+        (!candyMachine.data.goLiveDate ||
+            candyMachine.data.goLiveDate.toNumber() > new Date().getTime() / 1000);
+        
+        // We will be using this later in our UI so let's generate this now
+        const goLiveDateTimeString = `${new Date(
+            goLiveData * 1000
+          ).toLocaleDateString()} - ${new Date(
+            goLiveData * 1000
+          ).toLocaleTimeString()}`;
+
+          setCandyMachine({
+            id: process.env.NEXT_PUBLIC_CANDY_MACHINE_ID,
+            program,
+            state: {
+              itemsAvailable,
+              itemsRedeemed,
+              itemsRemaining,
+              goLiveData,
+              goLiveDateTimeString,
+              isSoldOut: itemsRemaining === 0,
+              isActive:
+                (presale ||
+                  candyMachine.data.goLiveDate.toNumber() < new Date().getTime() / 1000) &&
+                (candyMachine.endSettings
+                  ? candyMachine.endSettings.endSettingType.date
+                    ? candyMachine.endSettings.number.toNumber() > new Date().getTime() / 1000
+                    : itemsRedeemed < candyMachine.endSettings.number.toNumber()
+                  : true),
+              isPresale: presale,
+              goLiveDate: candyMachine.data.goLiveDate,
+              treasury: candyMachine.wallet,
+              tokenMint: candyMachine.tokenMint,
+              gatekeeper: candyMachine.data.gatekeeper,
+              endSettings: candyMachine.data.endSettings,
+              whitelistMintSettings: candyMachine.data.whitelistMintSettings,
+              hiddenSettings: candyMachine.data.hiddenSettings,
+              price: candyMachine.data.price,
+            },
+          }); 
+
+        console.log({
+        itemsAvailable,
+        itemsRedeemed,
+        itemsRemaining,
+        goLiveData,
+        goLiveDateTimeString,
+        presale,
+        });
+  };
 
     return (
         candyMachine && (
